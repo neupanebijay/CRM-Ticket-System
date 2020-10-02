bde6822a5a1d6a7d38e2b6e0fb3ea64ed33f8466
neupanebijay <neupanebijay56@gmail.com>
2020-10-02 15:43:04 +1000

    App setup, login form and reset Password

--- src/components/Password Reset/ResetPassword.js
+++ src/components/Password Reset/ResetPassword.js
@@ -2,21 +2,20 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
 
-export const LoginForm = ({
+export const ResetPassword = ({
   handleOnChange,
-  handleOnSubmit,
+  handleOnResetSubmit,
   formSwitch,
   email,
-  pass,
 }) => {
   return (
     <Container>
       <Row>
         <Col>
-          <h1 className="text-info text-center">Client Login</h1>
+          <h1 className="text-info text-center">Reset Password</h1>
           <hr />
           {/* createing a form */}
-          <form autoComplete="off" onSubmit={handleOnSubmit}>
+          <form autoComplete="off" onSubmit={handleOnResetSubmit}>
             <Form.Group>
               <Form.Label>Email Address</Form.Label>
               <Form.Control
@@ -28,19 +27,9 @@ export const LoginForm = ({
                 required
               />
             </Form.Group>
-            <Form.Group>
-              <Form.Label>Password</Form.Label>
-              <Form.Control
-                type="password"
-                name="password"
-                value={pass}
-                onChange={handleOnChange}
-                placeholder="*******"
-                required
-              />
-            </Form.Group>
+
             <Button variant="info" type="Submit">
-              Login
+              Reset Password
             </Button>
           </form>
           <hr />
@@ -48,8 +37,8 @@ export const LoginForm = ({
       </Row>
       <Row>
         <Col>
-          <a href="#!" onClick={() => formSwitch("reset")}>
-            Forget Password?
+          <a href="#!" onClick={() => formSwitch("login")}>
+            Login Now
           </a>
         </Col>
       </Row>
@@ -57,10 +46,10 @@ export const LoginForm = ({
   );
 };
 
-LoginForm.propTypes = {
+ResetPassword.propTypes = {
   handleOnChange: PropTypes.func.isRequired,
-  handleOnSubmit: PropTypes.func.isRequired,
+  handleOnResetSubmit: PropTypes.func.isRequired,
   formSwitch: PropTypes.func.isRequired,
   email: PropTypes.string.isRequired,
-  pass: PropTypes.string.isRequired,
+  // pass: PropTypes.string.isRequired,
 };
--- src/pages/entry/Entry.pages.js
+++ src/pages/entry/Entry.pages.js
@@ -1,13 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Entry.style.css";
 import { Jumbotron } from "react-bootstrap";
 import { LoginForm } from "../../components/login/Login.comp";
+import { ResetPassword } from "../../components/Password Reset/ResetPassword";
 
 export const Entry = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  //creating a useState to load reset password form in different page to load the loginform first
+  const [formLoad, setFormLoad] = useState("login");
+
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+    switch (name) {
+      case "email":
+        setEmail(value);
+        break;
+
+      case "password":
+        setPassword(value);
+        break;
+      default:
+        break;
+    }
+    // console.log(name, value);
+  };
+
+  const handleOnSubmit = (e) => {
+    // prevent default helps to stop the data(eg email ) disapper when page is refreshed.
+    e.preventDefault();
+
+    if (!email || password.length < 8) {
+      return alert(
+        "Please fill email propely and password should be of minimum 8 characters"
+      );
+    }
+    // To call API to submit the form
+    console.log(email, password);
+  };
+  const handleOnResetSubmit = (e) => {
+    // prevent default helps to stop the data(eg email ) disapper when page is refreshed.
+    e.preventDefault();
+
+    if (!email) {
+      return alert("Please fill email propely");
+    }
+    // To call API to submit the form
+    console.log(email);
+  };
+
+  const formSwitch = (formType) => {
+    setFormLoad(formType);
+  };
+
   return (
     <div className="entry-page bg-secondary">
       <Jumbotron className="form-box">
-        <LoginForm />
+        {formLoad === "login" && (
+          <LoginForm
+            handleOnChange={handleOnChange}
+            handleOnSubmit={handleOnSubmit}
+            formSwitch={formSwitch}
+            email={email}
+            pass={password}
+          />
+        )}
+
+        {formLoad === "reset" && (
+          <ResetPassword
+            handleOnChange={handleOnChange}
+            handleOnResetSubmit={handleOnResetSubmit}
+            formSwitch={formSwitch}
+            email={email}
+          />
+        )}
       </Jumbotron>
     </div>
   );
